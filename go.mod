2e34c46e6dbbcfd32a4dd0e23d1075025c15222a
chander <jpochander@gmail.com>
2021-09-17 11:42:19 +0300

    add modules, random

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gitchander/liteut
+
+go 1.16
---
+++ random/random.go
@@ -0,0 +1,52 @@
+package random
+
+import (
+	"math/rand"
+	"time"
+)
+
+func NewRandSeed(seed int64) *rand.Rand {
+	return rand.New(rand.NewSource(seed))
+}
+
+func NewRandTime(t time.Time) *rand.Rand {
+	return NewRandSeed(t.UnixNano())
+}
+
+func NewRandNow() *rand.Rand {
+	return NewRandTime(time.Now())
+}
+
+func Bool(r *rand.Rand) bool {
+	return ((r.Int() & 1) == 1)
+}
+
+func Byte(r *rand.Rand) byte {
+	return byte(r.Uint32())
+}
+
+func FillBytes(r *rand.Rand, bs []byte) {
+	const (
+		bitsPerByte    = 8
+		bytesPerUint64 = 8
+	)
+	var x uint64
+	var n int // number of random bytes
+	for i := range bs {
+		if n == 0 {
+			x = r.Uint64()
+			n = bytesPerUint64
+		}
+		bs[i] = byte(x)
+		x >>= bitsPerByte
+		n--
+	}
+}
+
+// IntMinMax gets pseudo-random number in range [min..max).
+func IntMinMax(r *rand.Rand, min, max int) int {
+	if min >= max {
+		panic("IntMinMax: invalid interval")
+	}
+	return min + r.Intn(max-min)
+}
