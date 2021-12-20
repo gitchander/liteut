8495389ee15fd424e12f169d15c63a52ecff6f62
chander <jpochander@gmail.com>
2021-12-20 17:32:02 +0200

    add new to random

--- random/random.go
+++ random/random.go
@@ -2,20 +2,7 @@ package random
 
 import (
 	"math/rand"
-	"time"
 )
-
-func NewRandSeed(seed int64) *rand.Rand {
-	return rand.New(rand.NewSource(seed))
-}
-
-func NewRandTime(t time.Time) *rand.Rand {
-	return NewRandSeed(t.UnixNano())
-}
-
-func NewRandNow() *rand.Rand {
-	return NewRandTime(time.Now())
-}
 
 func Bool(r *rand.Rand) bool {
 	return ((r.Int() & 1) == 1)
